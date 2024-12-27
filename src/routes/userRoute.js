494d565966df7a09767f840d109b227a0fc6f909
Dev Nia <hello.yudana@gmail.com>
2024-12-27 16:19:28 +0700

    fixed: feature crud admin

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,12 +6,12 @@ const authenticateJWT = require('../middleware/auth.js');
 
 const router = express.Router();
 
-router.post('/users',authenticateJWT, userController.createUser);
+router.post('/users', userController.createUser);
 router.get('/users', userController.getUser);
-router.get('/users/account/number/:accountNumber',authenticateJWT, userController.getUserByAccountNumber);
-router.get('/users/account/id/:identityNumber',authenticateJWT, userController.getUserByIdentityNumber);
-router.put('/users/account/:id',authenticateJWT, userController.updateById);
-router.delete('/users/account/:id',authenticateJWT, userController.deleteUserById);
+router.get('/users/account/number/:accountNumber', userController.getUserByAccountNumber);
+router.get('/users/account/id/:identityNumber', userController.getUserByIdentityNumber);
+router.put('/users/account/:id', userController.updateById);
+router.delete('/users/account/:id', userController.deleteUserById);
 
 
 module.exports = router;
